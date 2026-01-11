62228e3dc677d3866ce4d56a4f1ac0c623a5f787
Jemmesson <js.silva10@hotmail.com>
2026-01-11 20:57:00 -0300

    Exercicio com Encapsulamento

---
+++ ExercicioComEnpasulamento/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ExercicioComEnpasulamento
+{
+    internal class Produto
+    {
+        private string _nome; //Propriedade privade
+        public double Preco {  get; private set; } //Propriedade autoimplementadas
+        public int Quantidade { get; private set; } //Propriedade autoimplementadas
+
+        //Construtores
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Propriedade Customizada
+        public string Nome
+        {
+            get{ return _nome; }
+            
+            set 
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //Até o override é chamada de "Outros metodos da classe";
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", " +
+                "$ " + Preco.ToString("f2", CultureInfo.InvariantCulture) +
+                ", " + Quantidade + " unidades" +
+                ", " + "$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioComEnpasulamento/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace ExercicioComEnpasulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe a quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+           
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int add = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(add);
+            Console.WriteLine();
+            Console.Write("Dados Atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de prodrutos a ser removido do estoque: ");
+            int rmv = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(rmv);
+            Console.WriteLine();
+            Console.Write("Dados Atualizados: " + p);
+
+        }
+    }
+}
